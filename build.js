da0f668c719543237f491a2e295476f82b254429
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-05-14 14:46:15 +0200

    Exercice 5

--- build.js
+++ build.js
@@ -1,4 +1,7 @@
 const fs = require('fs-extra');
+const path = require('path');
+const globby = require('globby');
+const _ = require('lodash');
 
 process.chdir(__dirname);
 
@@ -7,6 +10,20 @@ async function rmAndMkdir() {
   await fs.mkdir('dest');
 }
 
-(async function () {
+async function copyImages() {
+  const srcImgPaths = await globby('src/**/*.{png,svg}');
+
+  for (const srcImgPath of srcImgPaths) {
+    const { name, ext } = path.parse(srcImgPath);
+
+    const destName = _.kebabCase(_.deburr(name));
+    const destImgPath = path.resolve('dest', destName) + ext;
+
+    await fs.copy(srcImgPath, destImgPath);
+  }
+}
+
+(async function() {
   await rmAndMkdir();
-})();
+  await copyImages();
+}());
