54b996ab609dfc3750505cbdf0a408a91355d03f
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-05-14 14:38:36 +0200

    Exercice 4

--- build.js
+++ build.js
@@ -1,1 +1,12 @@
+const fs = require('fs-extra');
+
 process.chdir(__dirname);
+
+async function rmAndMkdir() {
+  await fs.remove('dest');
+  await fs.mkdir('dest');
+}
+
+(async function () {
+  await rmAndMkdir();
+})();
